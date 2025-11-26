ab07fcc5b648294b37585facee365907c1273107
Daniel Rodríguez <dm0423051@gmail.com>
2025-11-25 21:07:30 -0500

    cambio en la base de datos para que se pueda realizar el despliegue

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     DB_HOST = os.environ.get('DB_HOST')
     DB_NAME = os.environ.get('DB_NAME')
 
-    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Email (Gmail)
