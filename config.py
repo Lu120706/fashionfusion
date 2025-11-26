5fef1a72266ebab3f44fd2fa5b521a77d939dca8
Daniel Rodríguez <dm0423051@gmail.com>
2025-11-25 21:05:14 -0500

    cambio en la base de datos para que se pueda realizar el despliegue

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
 
-    # Base de datos MySQL
-    DB_USER = os.environ.get('DB_USER', 'root')
-    DB_PASS = os.environ.get('DB_PASS', '')
-    DB_HOST = os.environ.get('DB_HOST', 'localhost')
-    DB_NAME = os.environ.get('DB_NAME', 'fashion_fusion')
+    # Base de datos PostgreSQL
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASS = os.environ.get('DB_PASS')
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_NAME = os.environ.get('DB_NAME')
 
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Email (Gmail)
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'fashonfusion140@gmail.com'
-    MAIL_PASSWORD = 'szrf rqic mfsl xnxa'
-    MAIL_DEFAULT_SENDER = 'fashonfusion140@gmail.com'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME')
